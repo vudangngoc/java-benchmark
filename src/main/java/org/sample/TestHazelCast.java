a5d986367296ea047364cb67c3d4abf524473ec8
vudangngoc <vudangngoc@gmail.com>
2017-04-04 13:38:20 +0700

    update TestHazelCast

--- src/main/java/org/sample/TestHazelCast.java
+++ src/main/java/org/sample/TestHazelCast.java
@@ -1,7 +1,8 @@
 package org.sample;
 
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -12,43 +13,41 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
-import org.openjdk.jmh.annotations.Threads;
-
 import com.hazelcast.config.XmlConfigBuilder;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IMap;
+import com.hazelcast.core.IList;
 
 public class TestHazelCast {
 
 	@Benchmark
 	@BenchmarkMode(Mode.AverageTime)
 	@OutputTimeUnit(TimeUnit.NANOSECONDS)
-	@Threads(10)
-	public void concurrentHashMap(BenchMarkState state){
-		while(true)
-			if(state.chmWrite.size() < 100000)
-				state.chmWrite.put(UUID.randomUUID().toString(), UUID.randomUUID());
-			else
-				break;
+	public void linkedList(BenchMarkState state){
+		Iterator<String> temp = state.chmRead.iterator();
+		while(temp.hasNext())
+			temp.next();
+			
 	}
 	@Benchmark
 	@BenchmarkMode(Mode.AverageTime)
 	@OutputTimeUnit(TimeUnit.NANOSECONDS)
-	@Threads(10)
-	public void hazelcast(BenchMarkState state){
-		while(true)
-			if(state.mapWrite.size() < 100000)
-				state.mapWrite.put(UUID.randomUUID().toString(), UUID.randomUUID());
-			else
-				break;
+	public void hazelcastList(BenchMarkState state){
+		Iterator<String> temp = state.mapRead.iterator();
+		while(temp.hasNext())
+			temp.next();
 	}
 	@State(Scope.Benchmark)
 	public static class BenchMarkState {
 		@Setup(Level.Trial)
 		public void doSetup() {
-			mapWrite = instance.getMap("write");
-			mapRead = instance.getMap("read");
+			mapRead = instance.getList("read");
+			
+			while(mapRead.size() < 100000){
+				chmRead.push(UUID.randomUUID().toString());
+				mapRead.add(UUID.randomUUID().toString());
+			}
+
 		}
 		@TearDown
 		public void tearDown(){
@@ -56,9 +55,7 @@ public class TestHazelCast {
 		}
 
 		private final HazelcastInstance instance = Hazelcast.newHazelcastInstance(new XmlConfigBuilder().build());
-		public IMap<String, Object> mapWrite;
-		public IMap<String, Object> mapRead;
-		public ConcurrentHashMap<String, Object> chmRead = new ConcurrentHashMap<>();
-		public ConcurrentHashMap<String, Object> chmWrite = new ConcurrentHashMap<>();
+		public IList<String> mapRead;
+		public LinkedList<String> chmRead = new LinkedList<>();
 	}
 }
