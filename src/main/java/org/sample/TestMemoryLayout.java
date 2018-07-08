c2001a5f3336c03c08d937332a095ae779777f61
Vũ Đăng Ngọc <vudangngoc@gmail.com>
2018-07-08 08:26:24 +0700

    Update TestMemoryLayout.java

--- src/main/java/org/sample/TestMemoryLayout.java
+++ src/main/java/org/sample/TestMemoryLayout.java
@@ -1,10 +1,7 @@
 package org.sample;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -14,7 +11,6 @@ import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
 
 public class TestMemoryLayout {
 
@@ -24,8 +20,8 @@ public class TestMemoryLayout {
 	public List<Integer> rowWise(BenchMarkState state){
 		int size = state.testData.length;
 		List<Integer> result = new ArrayList<>(size);		
-		for(int i = 0; i<1024; i++) 
-			for(int j = 0; j < size; j ++)
+		for(int i = 0; i< 1024; i++) 
+			for(int j = 0; j < 1024; j ++)
 				result.add(state.testData[i][j]);
 		return result;
 	}
@@ -35,15 +31,16 @@ public class TestMemoryLayout {
 	public List<Integer> columnWise(BenchMarkState state){
 		int size = state.testData.length;
 		List<Integer> result = new ArrayList<>(size);		
-		for(int j = 0; j < size; j ++)
-			for(int i = 0; i<1024; i++)
+		for(int j = 0; j < 1024; j ++)
+			for(int i = 0; i < 1024; i++)
 				result.add(state.testData[i][j]);
 
 		return result;
 	}
+
 //	Benchmark                    Mode  Cnt   Score   Error  Units
-//	TestMemoryLayout.rowWise     avgt  200   8.744 ± 0.127  ms/op
-//	TestMemoryLayout.columnWise  avgt  200  15.429 ± 0.102  ms/op
+//	TestMemoryLayout.columnWise  avgt  200  15.179 ± 0.087  ms/op
+//	TestMemoryLayout.rowWise     avgt  200   8.395 ± 0.104  ms/op
 	@State(Scope.Benchmark)
 	public static class BenchMarkState {
 		@Setup(Level.Trial)
@@ -53,7 +50,6 @@ public class TestMemoryLayout {
 					testData[i][j] = 0;
 
 		}
-
 		public int[][] testData = new int[1024][1024];
 	}
 
